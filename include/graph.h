820968a348532da918a7eec2051afe9ffb23589a
Ivan <ivan.shevchenko@ucu.edu.ua>
2025-02-21 17:36:30 +0100

    [ILP] Rewrote ILP implementation using BGL

--- include/graph.h
+++ include/graph.h
@@ -5,9 +5,18 @@
 #include <boost/graph/graphviz.hpp>
 #include <boost/graph/adjacency_list.hpp>
 
-struct Vertex {
+struct VertexStruct {
     std::string name;
+    std::string location;
+    size_t order;
+    size_t idx;
 };
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, Vertex> Graph;
+struct EdgeStruct {
+    std::string color;
+    size_t idx;
+};
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, VertexStruct, EdgeStruct> Graph;
+typedef Graph::vertex_descriptor Vertex;
+typedef Graph::edge_descriptor Edge;
 
 #endif //OKP_RECOGNITION_GRAPH_H
--- src/ilp.cpp
+++ src/ilp.cpp
@@ -2,15 +2,25 @@
 #include <vector>
 #include <algorithm>
 #include <gurobi_c++.h>
-#include <ogdf/fileformats/GraphIO.h>
 #include "solvers.h"
 
+void set_vertex_idx(Graph &graph);
+
+void set_edge_idx(Graph &graph);
 
 void ILPSolver(SolverParams &solverParams) {
-    if (solverParams.graph.numberOfNodes() <= 3) {
-        for (ogdf::node node: solverParams.graph.nodes) {
-            solverParams.ordering.push_back(node);
+    Graph &graph = solverParams.graph;
+    std::vector<Vertex> &ordering = solverParams.ordering;
+
+    unsigned long num_vertices = boost::num_vertices(graph);
+    ordering.clear();
+
+    if (num_vertices <= 3) {
+        for (Vertex node: boost::make_iterator_range(boost::vertices(graph))) {
+            ordering.push_back(node);
         }
+        solverParams.number_of_crossings = 0;
+        solverParams.converged = true;
         return;
     }
 
@@ -21,33 +31,31 @@ void ILPSolver(SolverParams &solverParams) {
 
     GRBModel model = GRBModel(env);
 
-    const ogdf::Graph &graph = solverParams.graph;
-
     // Order variables initialization
-    ogdf::NodeArray<ogdf::NodeArray<GRBLinExpr>> variables(graph);
-    for (const ogdf::node &node: graph.nodes) {
-        variables[node].init(graph, 0);
+    std::vector<std::vector<GRBLinExpr>> variables(num_vertices);
+    for (int i = 0; i < num_vertices; ++i) {
+        variables[i].resize(num_vertices, 0);
     }
-    for (const ogdf::node &start: graph.nodes) {
-        for (const ogdf::node &end: graph.nodes) {
-            if (ogdf::NodeElement::equal(*start, *end)) {
-                variables[start][end] = 0;
-            } else if (variables[end][start].size() == 0) {
-                variables[start][end] = model.addVar(
+    for (int i = 0; i < num_vertices; ++i) {
+        for (int j = 0; j < num_vertices; ++j) {
+            if (i == j) {
+                continue;
+            } else if (variables[j][i].size() == 0) {
+                variables[i][j] = model.addVar(
                         0, 1, 0, GRB_BINARY,
-                        "is_" + std::to_string(start->index()) +
-                        "_before_" + std::to_string(end->index())
+                        "is_" + std::to_string(i) +
+                        "_before_" + std::to_string(j)
                 );
             } else {
-                variables[start][end] = 1 - variables[end][start];
+                variables[i][j] = 1 - variables[j][i];
             }
         }
     }
 
     // Transitivity constraints
-    for (const ogdf::node &start: graph.nodes) {
-        for (const ogdf::node &end: graph.nodes) {
-            for (const ogdf::node &middle: graph.nodes) {
+    for (int start = 0; start < num_vertices; ++start) {
+        for (int middle = 0; middle < num_vertices; ++middle) {
+            for (int end = 0; end < num_vertices; ++end) {
                 model.addConstr(
                         variables[start][end] >=
                         variables[start][middle] +
@@ -57,26 +65,25 @@ void ILPSolver(SolverParams &solverParams) {
         }
     }
 
+    size_t num_edges = boost::num_edges(graph);
     GRBLinExpr crossing_upper_bound = model.addVar(0, GRB_INFINITY, 0, GRB_INTEGER, "k");
-    ogdf::EdgeArray<GRBLinExpr> edge_crossing_numbers(graph, 0);
+    set_vertex_idx(graph);
+    set_edge_idx(graph);
+    std::vector<GRBLinExpr> edge_crossing_numbers(num_edges);
 
     // Edge crossings variables definition
-    for (const ogdf::edge &edge1: graph.edges) {
-        ogdf::node u = edge1->source();
-        ogdf::node v = edge1->target();
-        for (const ogdf::edge &edge2: graph.edges) {
-            if (ogdf::EdgeElement::geq(*edge1, *edge2)) continue;
-            ogdf::node s = edge2->source();
-            ogdf::node t = edge2->target();
+    for (Edge edge1 : boost::make_iterator_range(boost::edges(graph))) {
+        size_t u = graph[boost::source(edge1, graph)].idx;
+        size_t v = graph[boost::target(edge1, graph)].idx;
+        for (Edge edge2 : boost::make_iterator_range(boost::edges(graph)))  {
+            if (graph[edge1].idx >= graph[edge2].idx) continue;
+            size_t s = graph[boost::source(edge2, graph)].idx;
+            size_t t = graph[boost::target(edge2, graph)].idx;
 
-            GRBVar do_cross = model.addVar(
-                    0, 1, 0, GRB_BINARY,
-                    "does_e" + std::to_string(edge1->index()) +
-                    "_cross_e" + std::to_string(edge2->index())
-            );
+            GRBVar do_cross = model.addVar(0, 1, 0, GRB_BINARY,"");
 
-            edge_crossing_numbers[edge1] += do_cross;
-            edge_crossing_numbers[edge2] += do_cross;
+            edge_crossing_numbers[graph[edge1].idx] += do_cross;
+            edge_crossing_numbers[graph[edge2].idx] += do_cross;
 
             {
                 model.addConstr(do_cross >= variables[u][s] + variables[s][v] + variables[v][t] - 2);
@@ -90,7 +97,7 @@ void ILPSolver(SolverParams &solverParams) {
             }
         }
         model.addConstr(crossing_upper_bound >=
-                        edge_crossing_numbers[edge1]);
+                        edge_crossing_numbers[graph[edge1].idx]);
     }
 
 #ifndef ILP_CROSSING_SUM_OPTIMIZATION
@@ -100,24 +107,33 @@ void ILPSolver(SolverParams &solverParams) {
     for (const GRBLinExpr &edge_sum: edge_crossing_numbers) {
         crossing_sum += edge_sum;
     }
-    int factor = graph.numberOfEdges() * graph.numberOfEdges();
+    size_t factor = num_edges * num_edges;
     model.setObjective(crossing_upper_bound + crossing_sum / factor, GRB_MINIMIZE);
 #endif
 
     model.optimize();
     solverParams.converged = true;
+    ordering.insert(ordering.end(), boost::vertices(graph).first, boost::vertices(graph).second);
 
-    std::vector<ogdf::node> &ordering = solverParams.ordering;
-    ordering.clear();
-    ordering.reserve(graph.numberOfNodes());
-    for (const ogdf::node &node: graph.nodes) {
-        ordering.push_back(node);
-    }
-    std::sort(ordering.begin(), ordering.end(), [&variables](const ogdf::node &u, const ogdf::node &v) {
-        return variables[u][v].getValue();
+    std::sort(ordering.begin(), ordering.end(), [&variables, &graph](Vertex u, Vertex v) {
+        return variables[graph[u].idx][graph[v].idx].getValue();
     });
 
     solverParams.number_of_crossings = crossing_upper_bound.getValue();
+}
+
+void set_vertex_idx(Graph &graph) {
+    size_t i = 0;
+    for (Vertex e: boost::make_iterator_range(boost::vertices(graph))) {
+        graph[e].idx = i++;
+    }
+}
+
+void set_edge_idx(Graph &graph) {
+    size_t i = 0;
+    for (Edge e: boost::make_iterator_range(boost::edges(graph))) {
+        graph[e].idx = i++;
+    }
 }
 
 
