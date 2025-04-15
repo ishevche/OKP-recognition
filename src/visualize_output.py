518d8873e8a5d136fb5c47b21cb6cbb02cddd349
Ivan <ivan.shevchenko@ucu.edu.ua>
2025-04-15 10:26:51 +0300

    [VIS] Updated visualisation script

--- src/visualize_output.py
+++ src/visualize_output.py
@@ -1,42 +1,63 @@
-import re
+#! /usr/bin/env python3
 import argparse
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import pandas as pd
-from collections import defaultdict
+
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("infiles", nargs="+", help="Input file to include",
-                        type=str)
-    parser.add_argument("-o", "--output", dest="outfile", default="data/out/output.png",
-                         help="Output file", type=str)
+    parser.add_argument("infile", default="data/results.csv",
+                        help="File that contains results of the experiment", type=str)
+    parser.add_argument("-o", "--output", dest="outfile", default="data/out/output.svg",
+                        help="File in which the plot is saved", type=str)
     return parser.parse_args()
 
-def read_data(file_path):
-    with open(file_path, "r") as file:
-        data = file.readlines()
-    stats = []
-    graph_data = ""
-    for line in data:
-        data_match = re.match(r'^(.*): (\d+) (\d+)\n$', line)
-        graph_match = re.match(r'^\s*\d+: (.*) \((\d+)\)\n$', line)
-        if data_match:
-            stats.append(graph_data + [data_match.group(1), int(data_match.group(2)), int(data_match.group(3))])
-        elif graph_match:
-            graph_data = [graph_match.group(1), int(graph_match.group(2))]
-    return pd.DataFrame(stats, columns=["graph", "bicomponents", "method", "k", "time"])
+def sync_cr(data):
+    data['ilp_cr'] = data['ilp_cr'].where(data['ilp_success'], np.nan)
+    data['okp_cr'] = data['okp_cr'].where(data['okp_success'], np.nan)
+    data['sat_cr'] = data['sat_cr'].where(data['sat_success'], np.nan)
+    rows = data[['ilp_cr', 'okp_cr', 'sat_cr']].dropna(how='all')
+    diff_cr = rows.min(axis=1) != rows.max(axis=1)
+    if diff_cr.any():
+        raise ValueError(f"Non consistent crossing numbers:\n{rows[diff_cr]}")
+    cr = data[['ilp_cr', 'okp_cr', 'sat_cr']].min(axis=1)
+    data['ilp_cr'] = cr
+    data['okp_cr'] = cr
+    data['sat_cr'] = cr
+    return data
 
-def plot_data(data, x, y, hue, outfile):
-    sns.boxplot(x=x, y=y, hue=hue, data=data, showfliers=False,
-                   palette=sns.cubehelix_palette(data[hue].nunique()))
-    plt.savefig(outfile)
-    plt.clf()
+def read_input(input_files):
+    data = pd.read_csv(input_files)
+    data = sync_cr(data)
+    data = pd.wide_to_long(data, stubnames=['ilp', 'sat', 'okp'],
+                           i=['graph_dot', 'graph_g6'],
+                           j='attribute', sep='_', suffix='.+').reset_index()
+    data = data.melt(id_vars=['graph_dot', 'graph_g6', 'attribute'],
+                     value_vars=['ilp', 'sat', 'okp'],
+                     var_name="method")
+    data = data.pivot(index=['graph_dot', 'graph_g6', 'method'],
+                      columns='attribute', values='value').reset_index()
+    # data = data[data['success'].notna()]
+    data = data[data['success'] == True]
+    # data = data[data['cr'] <= 7]
+    data['time'] /= 10 ** 9
+    data['time'] = data['time'].replace(0., 60.*10.)
+    data = data.astype({"cr": "int64"})
+    return data
 
 def main(input_files, output_file):
-    data = pd.concat([read_data(file) for file in input_files])
-    plot_data(data, "bicomponents", "time", "method", output_file)
+    data = read_input(input_files)
+    sns.boxplot(x="cr", y="time", hue='method', data=data, showfliers=False,
+                palette=sns.cubehelix_palette(data['method'].nunique()),
+                hue_order=["ilp", "sat", "okp"])
+    plt.xlabel("Crossing number")
+    plt.ylabel("Time, s")
+    plt.yscale('log')
+    plt.savefig(output_file)
+    plt.clf()
 
 if __name__ == "__main__":
     args = get_arguments()
-    main(args.infiles, args.outfile)
+    main(args.infile, args.outfile)
